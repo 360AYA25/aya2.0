d0c20275329628456683ea6e4d125188781e49fe
Sergey No <sergey@MacBook-Air-Sergey.local>
2025-06-20 19:03:05 -0400

    fix: proper CORSMiddleware call

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.telegram_adapter import router as tg_router
 
 app = FastAPI()
-app.add_middleware(
-)
 
-app.mount("/files", StaticFiles(directory="local_uploads"), name="files")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(tg_router)
 
